eb694c0d727588c37658d13ab69426e161d17d8e
JoaquinHubner <joaquinhubner42@gmail.com>
2025-04-05 18:44:00 -0300

    Update backend

--- OnlyBackend/index.js
+++ OnlyBackend/index.js
@@ -17,6 +17,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -48,10 +51,10 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
 
@@ -60,28 +63,27 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
+
   const { content, important } = request.body
 
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (!note) {
-        return response.status(404).end()
-      }
+  Note.findByIdAndUpdate(
+    request.params.id, 
 
-      note.content = content
-      note.important = important
-
-      return note.save().then((updatedNote) => {
-        response.json(updatedNote)
-      })
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
